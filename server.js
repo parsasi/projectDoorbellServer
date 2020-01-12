f289a21131d5c55a7d0bb892bb1cc15cd36524a5
parsasi <parsasi@rocketmail.com>
2020-01-11 22:36:48 -0800

    uploading works

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const formidable = require('formidable'),
 http = require('http'),
 fs = require('fs');
+const tmp = require('tmp');
+const { resolve, basename } = require('path');
 // DB = require('./db');
 // const port = '8080' , host = '127.0.0.1';
 const server = http.createServer(async (req,resp) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    resp.setHeader('Access-Control-Allow-Origin', '*');
     console.log('Reqested');
     try{
+        console.log(req.method, req.url);
         if(req.url === '/'){
             if(req.method.toLowerCase() == 'post'){
-                resp.statusCode = '200';
+                resp.statusCode = '200';    
                 resp.setHeader('content-type' , 'text/json');
                 let form = new formidable.IncomingForm();
+                form.uploadDir = resolve(__dirname, 'upload');
                 form.parse(req, async (err, fields, files)  => {
-                    if(!files.image){
+                    if(!files.webcam){
                         resp.statusCode = '200';
                         resp.setHeader('content-type' , 'text/json');
                         resp.end(JSON.stringify({code : 1 , msg : 'No Image sent!'}));
                         console.log('No images');
                     }else{
-                        const filePath = files.image.path;
-                        const fileName =  files.image.name;
-                        const destination = __dirname + '/uploads/' + fileName;
-                        await fs.rename(filePath, destination,  async (err) => {
-                            if (err) throw err;
-                            // let lables = await visionAPI(destination);
-                            resp.statusCode = 200;
-                            resp.setHeader('content-type', ' text/plain');
-                            console.log(upload);
-                            resp.end(JSON.stringify({code : 0}));
-                        });
+
+                        console.dir(files.webcam.path);
+                        const uploadedFileName = basename(files.webcam.path);
+                        resp.statusCode = '200';
+                        resp.setHeader('content-type' , 'text/json');
+                        resp.end(JSON.stringify({code : 1 , msg : 'Images saved' , url :  uploadedFileName}));
                     } 
                 });
             }
@@ -39,17 +38,22 @@ const server = http.createServer(async (req,resp) => {
                 resp.end('Doorbell Server !');
                 console.log('Get request');
             }    
+        } else if (req.url.includes('/photo')) {
+            const file = req.url.split('/')[2];
+            console.log(file + '!!!')
+
+            const stream = fs.createReadStream(resolve(__dirname, 'upload', file));
+
+            stream.pipe(resp);
         }
     }catch(e) {
         resp.statusCode = '200';
         resp.setHeader('content-type' , 'text/json');
-        resp.end(JSON.stringify({code : 1 , msg : e}));
+        resp.end(JSON.stringify({code : 1 , msg : e.message}));
         console.log('error');
     }
 });
-// server.listen(process.env.PORT);
 const port = process.env.PORT || process.env.WEBSITES_PORT || 8080;
-console.log('hi')
 server.listen(port ,(err) => {
     if(!err){
         console.log('Server is listening on  ' + port);
