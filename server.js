761a801dd26636371cc05d1eb9e8fb87a698ef2b
parsasi <parsasi@rocketmail.com>
2020-01-11 17:13:01 -0800

    Changes made

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const formidable = require('formidable'),
 http = require('http'),
-fs = require('fs'),
-DB = require('./db');
+fs = require('fs');
+// DB = require('./db');
 const port = '8080' , host = '127.0.0.1';
 const server = http.createServer(async (req,resp) => {
     try{
@@ -11,18 +11,29 @@ const server = http.createServer(async (req,resp) => {
                 resp.setHeader('content-type' , 'text/json');
                 let form = new formidable.IncomingForm();
                 form.parse(req, async (err, fields, files)  => {
-                    const filePath = files.image.path;
-                    const fileName =  files.image.name;
-                    const destination = __dirname + '/uploads/' + fileName;
-                    await fs.rename(filePath, destination,  async (err) => {
-                        if (err) throw err;
-                        // let lables = await visionAPI(destination);
-                        resp.statusCode = 200;
-                        resp.setHeader('content-type', ' application/json');
-                        resp.end(JSON.stringify({code : 0}));
-                    });
+                    if(!files.image){
+                        resp.statusCode = '200';
+                        resp.setHeader('content-type' , 'text/json');
+                        resp.end(JSON.stringify({code : 1 , msg : 'No Image sent!'}));
+                    }else{
+                        const filePath = files.image.path;
+                        const fileName =  files.image.name;
+                        const destination = __dirname + '/uploads/' + fileName;
+                        await fs.rename(filePath, destination,  async (err) => {
+                            if (err) throw err;
+                            // let lables = await visionAPI(destination);
+                            resp.statusCode = 200;
+                            resp.setHeader('content-type', ' application/json');
+                            resp.end(JSON.stringify({code : 0}));
+                        });
+                    } 
                 });
             }
+            else{
+                resp.statusCode = '200';
+                resp.setHeader('content-type' , 'text/json');
+                resp.end(JSON.stringify({code : 1 , msg : 'Get request sent!'}));
+            }    
         }
     }catch(e) {
         resp.statusCode = '200';
