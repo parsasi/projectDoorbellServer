1cc60f35ce57ae6463eee0d34c3afa250c5090a2
parsasi <parsasi@rocketmail.com>
2020-01-11 23:10:08 -0800

    FaceId is not implemented yet

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const formidable = require('formidable'),
 http = require('http'),
 fs = require('fs');
-const tmp = require('tmp');
+const fd = require('./face/facedetection');
 const { resolve, basename } = require('path');
 // DB = require('./db');
-// const port = '8080' , host = '127.0.0.1';
+const host = process.env.PUBLIC_URL || '127.0.0.1';
+const port = process.env.PORT || process.env.WEBSITES_PORT || 8080;
 const server = http.createServer(async (req,resp) => {
     resp.setHeader('Access-Control-Allow-Origin', '*');
-    console.log('Reqested');
+    console.log(req.url);
     try{
         console.log(req.method, req.url);
         if(req.url === '/'){
@@ -21,14 +22,11 @@ const server = http.createServer(async (req,resp) => {
                         resp.statusCode = '200';
                         resp.setHeader('content-type' , 'text/json');
                         resp.end(JSON.stringify({code : 1 , msg : 'No Image sent!'}));
-                        console.log('No images');
                     }else{
-
-                        console.dir(files.webcam.path);
                         const uploadedFileName = basename(files.webcam.path);
                         resp.statusCode = '200';
                         resp.setHeader('content-type' , 'text/json');
-                        resp.end(JSON.stringify({code : 1 , msg : 'Images saved' , url :  uploadedFileName}));
+                        resp.end(JSON.stringify({code : 1 , msg : 'Images saved' , url : host + ':' + port + '/' + uploadedFileName }));
                     } 
                 });
             }
@@ -38,7 +36,7 @@ const server = http.createServer(async (req,resp) => {
                 resp.end('Doorbell Server !');
                 console.log('Get request');
             }    
-        } else if (req.url.includes('/photo')) {
+        } else if (req.url.startsWith('/photo')) {
             const file = req.url.split('/')[2];
             console.log(file + '!!!')
 
@@ -53,17 +51,10 @@ const server = http.createServer(async (req,resp) => {
         console.log('error');
     }
 });
-<<<<<<< HEAD
-const port = process.env.PORT || process.env.WEBSITES_PORT || 8080;
 server.listen(port ,(err) => {
     if(!err){
         console.log('Server is listening on  ' + port);
     }else{
         console.log(err);
     }
-=======
-
-server.listen( port, host, (err) => {
-    err ? console.log(err) : console.log( `Server is live on ${port}` );
->>>>>>> 0a87ae3b4609355e42cfd079b30708edbe3aaf30
 });
