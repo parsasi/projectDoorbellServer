fc47efb4c2e7061b856cccd512f088517bf08af9
Maxine Yee <maxineyeee@gmail.com>
2020-01-12 01:31:34 -0800

    Push notifications, only work when app is foregrounded

---
+++ pusher/constants.js
@@ -0,0 +1,9 @@
+const instanceId = "1a6d3dff-6853-43cc-a83f-81679de029cf";
+const secretKey = "DA7E310F893F554222C2AA51F3CD34D809C6F9B67184608CE3F161E6E5C2D8FD";
+const interest = "hello"
+
+module.exports = {
+  instanceId,
+  secretKey,
+  interest
+}
