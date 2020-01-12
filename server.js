c43826c5dea9955ef2a6bb0f9c604e0f56120b32
parsasi <parsasi@rocketmail.com>
2020-01-11 20:46:33 -0800

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const formidable = require('formidable'),
 http = require('http'),
 fs = require('fs');
 // DB = require('./db');
-const port = '8080' , host = '127.0.0.1';
+// const port = '8080' , host = '127.0.0.1';
 const server = http.createServer(async (req,resp) => {
+    console.log('Reqested');
     try{
         if(req.url === '/'){
             if(req.method.toLowerCase() == 'post'){
@@ -15,6 +16,7 @@ const server = http.createServer(async (req,resp) => {
                         resp.statusCode = '200';
                         resp.setHeader('content-type' , 'text/json');
                         resp.end(JSON.stringify({code : 1 , msg : 'No Image sent!'}));
+                        console.log('No images');
                     }else{
                         const filePath = files.image.path;
                         const fileName =  files.image.name;
@@ -23,7 +25,8 @@ const server = http.createServer(async (req,resp) => {
                             if (err) throw err;
                             // let lables = await visionAPI(destination);
                             resp.statusCode = 200;
-                            resp.setHeader('content-type', ' text/json');
+                            resp.setHeader('content-type', ' text/plain');
+                            console.log(upload);
                             resp.end(JSON.stringify({code : 0}));
                         });
                     } 
@@ -31,18 +34,22 @@ const server = http.createServer(async (req,resp) => {
             }
             else{
                 resp.statusCode = '200';
-                resp.setHeader('content-type' , 'text/json');
-                resp.end(JSON.stringify({code : 1 , msg : 'Get request sent!'}));
+                resp.setHeader('content-type' , 'text/html');
+                resp.end('Doorbell Server !');
+                console.log('Get request');
             }    
         }
     }catch(e) {
         resp.statusCode = '200';
         resp.setHeader('content-type' , 'text/json');
         resp.end(JSON.stringify({code : 1 , msg : e}));
+        console.log('error');
     }
 });
-
-server.listen(port,host,(err) => {
+// server.listen(process.env.PORT);
+const port = process.env.PORT || process.env.WEBSITES_PORT || 8080;
+console.log('hi')
+server.listen(port ,(err) => {
     if(!err){
         console.log('Server is listening on  ' + port);
     }else{
